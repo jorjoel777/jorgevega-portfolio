76f3401903f782c78fbea2742e5ce9eb937420cc
Jorge Vega <jorbre@gmail.com>
2025-05-05 17:08:09 +0200

    fixed image and cv

--- src/components/About.vue
+++ src/components/About.vue
@@ -41,7 +41,7 @@
         />
 
         <a
-          href="/Jorge_Vega_Lead_Software_Engineer.pdf"
+          href="public\Jorge_Vega_Lead_Software_Engineer.pdf"
           download
           class="text-yellow-400 hover:text-cyan-400 text-xl font-semibold tracking-wide transition-colors duration-300"
         >
